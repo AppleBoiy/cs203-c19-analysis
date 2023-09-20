05a3edfc8e5f3c7d00be8497379ce19607ae43e9
Chaipat J <work.chaipat@gmail.com>
2023-09-20 22:21:24 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from src.downloader import DataDownloader
 
 if __name__ == "__main__":
-    data_downloader = DataDownloader(debug=True, write=True)
+    data_downloader = DataDownloader()
     data_downloader.start()
