740c90d3ad99efbf6039e966212379dcc4a150b6
Chaipat J <work.chaipat@gmail.com>
2023-09-20 21:50:16 +0700

    move LICENSE and README to .github folder

--- src/heatmap.py
+++ src/heatmap.py
@@ -3,7 +3,6 @@ import folium
 
 def display_heatmap(df, state_name) -> folium.Map | None:
     total_deaths_sum = df['Total Death'].sum()
-    total_confirmed_sum = df['Total Confirmed'].sum()
 
     # Filter the dataset based on the input state or province name
     state_df = df[df['Province/State'] == state_name]
@@ -14,7 +13,6 @@ def display_heatmap(df, state_name) -> folium.Map | None:
 
     # Calculate the Death Ratio and Confirmed Ratio for the selected state
     death_ratio = state_df.iloc[0]['Total Death'] / total_deaths_sum
-    confirmed_ratio = state_df.iloc[0]['Total Confirmed'] / total_confirmed_sum
 
     # Create a base map centered at the selected state's location
     m = folium.Map(location=[state_df.iloc[0]['lat'], state_df.iloc[0]['lon']], zoom_start=6)
@@ -28,17 +26,6 @@ def display_heatmap(df, state_name) -> folium.Map | None:
         fill_color='red',
         fill_opacity=0.6,
         popup=f"State: {state_name}<br>Death Ratio: {death_ratio:.2f}",
-    ).add_to(m)
-
-    # Create and add the Confirmed Ratio Heatmap
-    folium.CircleMarker(
-        location=(state_df.iloc[0]['lat'], state_df.iloc[0]['lon']),
-        radius=10 * confirmed_ratio,
-        color='blue',
-        fill=True,
-        fill_color='blue',
-        fill_opacity=0.6,
-        popup=f"State: {state_name}<br>Confirmed Ratio: {confirmed_ratio:.2f}",
     ).add_to(m)
 
     # Display the map in the Jupyter Notebook
