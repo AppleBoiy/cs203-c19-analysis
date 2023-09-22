9f2e65c7382f5de428b4fe9eb75985dde265f4b2
Chaipat J. <work.chaipat@gmail.com>
2023-09-22 21:21:10 +0700

    add test module

---
+++ test/test_plotter.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+from src.plotter import display_heatmap, histogram_plot, box_plot, density_plot
+
+
+sample_data = pd.DataFrame({
+    'Province/State': ['Alabama', 'Alabama', 'Alabama'],
+    'City/County/Borough/Region': ['City1', 'City2', 'City3'],
+    'Total Death': [10, 20, 30],
+    'Total Confirmed': [100, 200, 300],
+    'Death Rate': [0.1, 0.2, 0.3],
+    'lat': [34.7, 33.8, 35.0],
+    'lon': [-86.6, -87.2, -86.7],
+})
+
+
+def test_display_heatmap():
+    result = display_heatmap(sample_data, 'Alabama')
+    assert result is not None
+
+
+def test_histogram_plot():
+    path = 'images/histogram_plot.png'
+    histogram_plot(sample_data['Total Death'], 'Total Death', bins=5, path=path, debug=True)
+    assert os.path.exists(path)
+
+
+def test_box_plot():
+    path = 'images/box_plot.png'
+    box_plot(sample_data['Total Death'], 'Total Death', path=path, debug=True)
+    assert os.path.exists(path)
+
+
+def test_density_plot():
+    path = 'images/density_plot.png'
+    density_plot(sample_data['Total Death'], 'Total Death', path=path, debug=True)
+    assert os.path.exists(path)
