d9def392db3eb9fdc3f652face49d555bc7ec111
AppleBoiy <work.chaipat@gmail.com>
2023-10-11 13:00:03 +0700

    C_exploration 2_infected_behavior

--- code/C_exploration/format_data.py
+++ code/C_exploration/format_data.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
 
-def format_type(df):
-    if "FIPS" in df.columns:
-        df["FIPS"] = df["FIPS"].astype(str).str.replace(".0", "")
-
+def format_type(df, pre=False):
     # Date is not a string, it's a date
     df["date"] = pd.to_datetime(df["date"])
 
-    # Lat and Lon are not string, they are floated
-    df["lat"] = df["lat"].astype(float)
-    df["lon"] = df["lon"].astype(float)
+    if pre:
+        df["FIPS"] = df["FIPS"].astype(str).str.replace(".0", "")
+        # Lat and Lon are not string, they are floated
+        df["lat"] = df["lat"].astype(float)
+        df["lon"] = df["lon"].astype(float)
+    else:
+        df["cases"] = df["cases"].astype(int)
+        df["deaths"] = df["death"].astype(int)
+        df.drop(columns=["death"], inplace=True)
 
     if "id" in df.columns:
         df.set_index("id", inplace=True)
