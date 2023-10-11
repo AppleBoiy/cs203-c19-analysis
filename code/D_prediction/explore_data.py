b7be855f57be6f009b742065cd13a7ba25deea25
AppleBoiy <work.chaipat@gmail.com>
2023-10-11 17:52:12 +0700

    predict & use case

---
+++ code/D_prediction/explore_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+def get_data():
+    files = [
+        "spring1.csv",
+        "summer1.csv",
+        "fall1.csv",
+        "winter1.csv",
+        "spring2.csv",
+        "summer2.csv",
+        "fall2.csv",
+        "winter2.csv",
+    ]
+    data_dir = "../C_exploration/data"
+    data = pd.concat(
+        [pd.read_csv(f"{data_dir}/{file}") for file in files], ignore_index=True
+    )
+    return data
+
+
+def create_ready_to_use_data():
+    data = get_data()
+    data.drop(columns=["state"], inplace=True)
+    data.to_csv("data.csv", index=False)
+
+
+if __name__ == "__main__":
+    create_ready_to_use_data()
